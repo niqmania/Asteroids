769b7184eeaac875a92905f8da1f291a517b6c1b
niqmania <nik.find@yahoo.de>
2025-05-20 11:39:57 +0200

    Player Class and draw method

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     print("Screen width:", SCREEN_WIDTH)
     print("Screen height:", SCREEN_HEIGHT)
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -14,8 +20,12 @@ def main():
                 pygame.quit()
                 return
 
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+
+        # Limit the frame rate to 60 FPS
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
